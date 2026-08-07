215861239a0d3c3fafce5f26a02d1598b88f6067
AWRDev <arturk921@gmail.com>
2026-08-07 15:04:53 +0500

    BackoffPolicy was added for NetworkWorker

--- app/src/main/java/com/awrdev/white_list_tester/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/awrdev/white_list_tester/ui/home/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.awrdev.white_list_tester.ui.home
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.work.BackoffPolicy
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequestBuilder
@@ -9,6 +10,7 @@ import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.awrdev.white_list_tester.services.NetworkWorker
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.util.concurrent.TimeUnit
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 import kotlin.time.toJavaDuration
@@ -16,7 +18,13 @@ import kotlin.time.toJavaDuration
 //@HiltViewModel
 class HomeViewModel : ViewModel() {
     fun periodicRequest(context: Context){
-        val networkWorkRequest = PeriodicWorkRequestBuilder<NetworkWorker>(repeatInterval = 100.toDuration(DurationUnit.SECONDS).toJavaDuration()).build()
+        val networkWorkRequest = PeriodicWorkRequestBuilder<NetworkWorker>(repeatInterval = 100.toDuration(DurationUnit.SECONDS).toJavaDuration())
+            .setBackoffCriteria(
+                BackoffPolicy.LINEAR,             // LINEAR означает, что пауза будет фиксированной (+5 минут каждый раз)
+                5,                                // Длительность паузы
+                TimeUnit.MINUTES                  // Единица измерения времени (минуты)
+            )
+            .build()
         WorkManager.getInstance(context).enqueueUniquePeriodicWork("periodic",
             ExistingPeriodicWorkPolicy.REPLACE, networkWorkRequest)
     }
